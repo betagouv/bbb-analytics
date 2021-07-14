960aac7f4834eee819a666fabc92602ace18713b
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2021-07-14 15:31:44 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
-const isSecure = (process.env.SECURE || 'true') === 'true';
-
 module.exports = {
   secret: process.env.JWT_SECURITY_SALT,
-  secure: isSecure,
-  protocol: isSecure ? 'https' : 'http',
   host: process.env.HOSTNAME,
   port: process.env.PORT || 8100,
 };
